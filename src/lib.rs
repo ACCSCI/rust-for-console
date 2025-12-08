56809bd1344f62b755ddd7394c8c9478a8da6552
Marco Taylor <nonesimple@yandex.com>
2025-12-08 21:19:22 +0800

    修正模块名称拼写错误，将 `preclude` 更改为 `prelude`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // 这是一个技巧：re-export
 // 以后你在新项目里只要 `use my_rust_stack::prelude::*;`
 // 所有你喜欢的库就都自动引入了，不用写一堆 use
-pub mod preclude{
+pub mod prelude{
     pub use anyhow::{Result, Context};// 自动把 Result 变成 anyhow 的
     pub use text_io::{scan,read};
     pub use walkdir::WalkDir;
